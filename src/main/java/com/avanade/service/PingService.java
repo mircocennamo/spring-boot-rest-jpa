d77fe9175ee239e5f0aba7e356e23a69270f25f4
mirco.cennamo <mirco.cennamo@avanade.com>
2025-04-04 15:26:55 +0200

    aggiunta integrazione k8s

--- src/main/java/com/avanade/service/PingService.java
+++ src/main/java/com/avanade/service/PingService.java
@@ -2,13 +2,12 @@ package com.avanade.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author mirco.cennamo on 03/04/2025
  * @project spring-boot-rest-jpa
  */
-@FeignClient(name = "ping", url = "${ping.url}")
+@FeignClient(name = "ping-service")
 public interface PingService {
     @GetMapping("/api/ping")
     String getPingResponse();
